b005799811a4ac373da86547f332e594e180d83e
nevilrv <nevilrv@gmail.com>
2024-07-20 13:03:26 +0530

    add versal

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const path = require('path');
-const helmet = require('helmet');
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+const helmet = require("helmet");
 
-const errorMiddleware = require('./errors/error');
-dotenv.config({ path: 'config/config.env' });
+const errorMiddleware = require("./errors/error");
+dotenv.config({ path: "config/config.env" });
 
 const app = express();
-const socketManager = require('./utils/socketManager');                       
+app.use(cors());
+const socketManager = require("./utils/socketManager");
 const server = http.createServer(app);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 socketManager.initSocket(server);
 
-app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,16 +32,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 
 // Route imports
-const admin = require('./routes/adminRoute');
-const user = require('./routes/userRoutes');
-const staff = require('./routes/staffRoute');
+const admin = require("./routes/adminRoute");
+const user = require("./routes/userRoutes");
+const staff = require("./routes/staffRoute");
 
-app.use('/api/v1', admin);
-app.use('/api/v1', user);
-app.use('/api/v1', staff);
+app.use("/api/v1", admin);
+app.use("/api/v1", user);
+app.use("/api/v1", staff);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "html", "index.html"));
 });
 
 // Error
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const { app, server } = require('./app'); 
-const dotenv = require('dotenv');
-const connectDatabase = require('./database');
+const { app, server } = require("./app");
+const dotenv = require("dotenv");
+const connectDatabase = require("./database");
 
-dotenv.config({ path: 'config/config.env' });
-connectDatabase(); 
+dotenv.config({ path: "config/config.env" });
+connectDatabase();
 process.setMaxListeners(15);
 
 // Start the server
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 const serverInstance = server.listen(port, () => {
-    console.log(`Server is working on port ${port}`);
+  console.log(`Server is working on port ${port}`);
 });
 
-process.on('unhandledRejection', (err) => {
-    console.log(`Error: ${err.message}`);
-    console.log('Shutting down the server due to Unhandled Promise Rejection');
-    serverInstance.close(() => {
-        process.exit(1);
-    });
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to Unhandled Promise Rejection");
+  serverInstance.close(() => {
+    process.exit(1);
+  });
 });
