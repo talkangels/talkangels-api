e9bcbba9dc88bcf9b23806cb485cf26738f90dba
talkangels <155733941+talkangels@users.noreply.github.com>
2024-09-11 15:10:25 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const { authenticateUser, authorizePermission } = require("../middleware/auth")
 
 const { getAllAngels, getOneUser, applyReferralCode, getOneAngel, addReport, deleteUser } = require("../controller/userController/userController")
 const { generateAgoraInfoForUser, callRejectNotification } = require("../controller/userController/userCallController")
-const { getAllRecharges, addBallance } = require("../controller/userController/paymantController")
+const { getAllRecharges, ProceedPayment, VerifyPayment, getPaymentHistory, addBalance } = require("../controller/userController/paymantController")
 const { addRating } = require("../controller/userController/ratingController")
 const { logIn, logout } = require("../controller/logInController")
 
@@ -18,11 +18,15 @@ router.post("/call-reject", authenticateUser, callRejectNotification)
 
 router.get("/user/detail/:id", authenticateUser, authorizePermission("user"), getOneUser)
 router.get("/user/all-recharge", authenticateUser, authorizePermission("user"), getAllRecharges)
-router.post("/user/add-ballence", authenticateUser, authorizePermission("user"), addBallance)
 router.post("/user/apply-refer-code", authenticateUser, authorizePermission("user"), applyReferralCode)
 router.post("/user/add-rating", authenticateUser, authorizePermission("user"), addRating)
 router.get("/user/angel-detail/:id", authenticateUser, authorizePermission("user"), getOneAngel)
 router.post("/user/add-report", authenticateUser, authorizePermission("user"), addReport)
 router.delete("/user/delete/:id", authenticateUser, deleteUser)
+
+router.get("/user/create-payment/:user_id",  authenticateUser, authorizePermission("user"), ProceedPayment)
+router.get("/user/verify-payment/:order_id", authenticateUser, authorizePermission("user"),  VerifyPayment)
+router.post("/user/add-ballence",  authenticateUser, authorizePermission("user"),addBalance)
+router.get("/user/all-payment/:id", authenticateUser, authorizePermission("user"), getPaymentHistory)
 
 module.exports = router
