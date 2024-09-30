6cec2c8c397299d67c94aa14bf4faf4b6bef1c9e
talkangels <talkangels5524@gmail.com>
2024-09-30 17:31:51 +0530

    change the page limit

--- controller/adminController/user/userController.js
+++ controller/adminController/user/userController.js
@@ -4,34 +4,12 @@ const User = require("../../../models/userModel");
 
 const getAllUser = async (req, res, next) => {
     try {
-        const page = parseInt(req.query.page_no) || 1;
-        const perPage = parseInt(req.query.items_per_page) || 10;
-        const { search_text } = req.query;
-
-        const query = search_text ? { $or: [{ name: { $regex: search_text, $options: 'i' } }, { mobile_number: { $regex: search_text, $options: 'i' } }] } : {};
-        const skip = (page - 1) * perPage;
-
-        const user = await User.find(query)
-            .skip(skip)
-            .limit(perPage);
-
-        const totalUsers = await User.countDocuments(query);
-        const allUsers = await User.countDocuments()
-        const totalPages = Math.ceil(totalUsers / perPage);
-
+        const user = await User.find()
         return res.status(StatusCodes.OK).json({
             status: StatusCodes.OK,
             success: true,
             data: user,
-            pagination: {
-                total_items: allUsers,
-                total_pages: totalPages,
-                current_page_item: user.length,
-                page_no: parseInt(page),
-                items_per_page: parseInt(perPage),
-            },
         });
-
     } catch (error) {
         return next(new ErrorHandler(error, StatusCodes.INTERNAL_SERVER_ERROR));
     }
